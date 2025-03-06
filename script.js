a913d66ca32f46e675f5c8ca3812c7f1a232655d
GEKO-3 <149662809+GEKO-3@users.noreply.github.com>
2025-03-06 06:25:26 +0500

    touch controls\

--- script.js
+++ script.js
@@ -70,3 +70,63 @@ svgContainer.addEventListener('mouseup', () => {
 svgContainer.addEventListener('mouseleave', () => {
     isPanning = false;
 });
+
+let initialTouchDistance = 0;
+let initialTouchScale = 1;
+let initialTouchPanX = 0;
+let initialTouchPanY = 0;
+
+svgContainer.addEventListener('touchstart', (event) => {
+    event.preventDefault(); // Disable default touch controls
+    if (event.touches.length === 2) {
+        initialTouchDistance = getDistance(event.touches[0], event.touches[1]);
+        initialTouchScale = scale;
+        initialTouchPanX = panX;
+        initialTouchPanY = panY;
+        const touchCenter = getTouchCenter(event.touches[0], event.touches[1]);
+        startX = touchCenter.x - panX;
+        startY = touchCenter.y - panY;
+    } else if (event.touches.length === 1) {
+        isPanning = true;
+        startX = event.touches[0].clientX - panX;
+        startY = event.touches[0].clientY - panY;
+    }
+});
+
+svgContainer.addEventListener('touchmove', (event) => {
+    event.preventDefault(); // Disable default touch controls
+    if (event.touches.length === 2) {
+        const currentDistance = getDistance(event.touches[0], event.touches[1]);
+        const touchScale = currentDistance / initialTouchDistance;
+        const newScale = Math.max(1, Math.min(initialTouchScale * touchScale, 150));
+        const touchCenter = getTouchCenter(event.touches[0], event.touches[1]);
+        const offsetX = (touchCenter.x - panX) * (newScale / scale - 1);
+        const offsetY = (touchCenter.y - panY) * (newScale / scale - 1);
+
+        panX -= offsetX;
+        panY -= offsetY;
+
+        scale = newScale;
+        svgElement.style.transformOrigin = '0 0';
+        svgElement.style.transform = `scale(${scale}) translate(${panX / scale}px, ${panY / scale}px)`;
+    } else if (event.touches.length === 1 && isPanning) {
+        panX = event.touches[0].clientX - startX;
+        panY = event.touches[0].clientY - startY;
+        svgElement.style.transform = `scale(${scale}) translate(${panX / scale}px, ${panY / scale}px)`;
+    }
+});
+
+svgContainer.addEventListener('touchend', () => {
+    isPanning = false;
+});
+
+function getDistance(touch1, touch2) {
+    return Math.sqrt(Math.pow(touch2.clientX - touch1.clientX, 2) + Math.pow(touch2.clientY - touch1.clientY, 2));
+}
+
+function getTouchCenter(touch1, touch2) {
+    return {
+        x: (touch1.clientX + touch2.clientX) / 2,
+        y: (touch1.clientY + touch2.clientY) / 2
+    };
+}
